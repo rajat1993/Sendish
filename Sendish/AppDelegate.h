873c688c6ab7fa205dccb1e85d554d0bcb09912a
Rajat Sharma <rajatsharma@Rajats-MacBook-Pro.local>
2014-12-29 21:20:35 +0530

    Initial Commit

---
+++ Sendish/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sendish
+//
+//  Created by Rajat Sharma on 29/12/14.
+//  Copyright (c) 2014 Rajat Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sendish/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Sendish
+//
+//  Created by Rajat Sharma on 29/12/14.
+//  Copyright (c) 2014 Rajat Sharma. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
